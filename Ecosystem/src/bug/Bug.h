efe572922191d1bb3037d1d5282590c42260c336
Franck XU <akubrain@MacBook-Pro-de-Franck.local>
2023-03-17 17:02:03 +0100

    flemme

--- Ecosystem/src/bug/Bug.h
+++ Ecosystem/src/bug/Bug.h
@@ -11,44 +11,49 @@
 #include <vector>
 using namespace std;
 
-class IComportement;
-class Milieu;
-
 using Couleur = array<T, 3>;
+
+enum State {
+    EActive,
+    EPaused,
+    EDead
+};
+
+
 class Bestiole {
 
 private:
   // position variables
+  int id;
+
   int x;
   int y;
-  double vitesse;
+
+  double currentVelocity;
+  double normalVelocity;
+  double fastVelocity;
+
   double orientation;
-  double max_vitesse;
 
-  /**
-   * fragility = [0, 1] (Probabilite de mort)
-   */
-  double fragility;
+  int age;
 
-  double camouflage_coef;
+  double camouflageCapacity;
+  double cloneProbability;
+  double deathProbability;
 
   // affichage
   double cumulX, cumulY;
   std::vector<unique_ptr<ICapteur>> list_capteurs;
 
-  unique_ptr<IComportement> comportement;
+  Behavior* behavior;
 
-  bool alive = true;
+  State mState;
+
 
 public: // Forme canonique :
-  int identite;
-  // other variables
-  int age;
-  int age_limit;
 
-  Bestiole(int x, int y, double vitesse, double max_vitesse, int age_limit,
-           double fragility, double camouflage_coef,
-           double orientation); // Constructeur par defaut
+  Bestiole(int id, int x, int y, double normal_vitesse, double max_vitesse, int age_limit, double camouflage_coef,
+           double death_prob, double clone_prob, double orientation); // Constructeur par defaut
   Bestiole(const Bestiole &b);  // Constructeur de copies
   Bestiole(Bestiole &&b);       // Move constructeur
 
@@ -57,44 +62,44 @@ public: // Forme canonique :
   ~Bestiole(); // Destructeur                              // Operateur
                // d'affectation binaire par defaut
 
-  void bouge(int xLim, int yLim);
-
-  void addAccessoire(IAccessoire acc);
-  void addCapteur(ICapteur *capteur);
-  void setComportement(unique_ptr<IComportement> comportement);
-
-  void action(Milieu &monMilieu);
-
-  bool jeTePercoit(const Bestiole &b) const;
-  void changeState();
-  void draw(UImg &support);
-  double get_camouflage_coef() const;
-  void set_camouflage_coef(double) { this->camouflage_coef = camouflage_coef; };
-
-  double getOrientation() const;
-  void setOrientation(double o);
-
-  pair<double, double> getCoordinates() const;
-
-  void setVitesse(double o);
-  double get_vitesse() const { return this->vitesse; };
-  double get_max_vitesse() const { return this->max_vitesse; };
-
-  friend bool operator==(const Bestiole &b1, const Bestiole &b2);
-
-  void addAccessoire(double speed_multiplier, double fragility_multiplier,
-                     double camouflage_mutliplier);
-
-  void set_fragility(double f) { this->fragility = f; };
-  double get_fragility() { return this->fragility; };
-
-  void move_capteur(unique_ptr<ICapteur> &&cap);
-
-  bool isCollidingWith(Bestiole const &b) const;
-  bool isDead() const;
-  void kill();
-
-  string get_comportement_name() const;
+//  void bouge(int xLim, int yLim);
+//
+//  void addAccessoire(IAccessoire acc);
+//  void addCapteur(ICapteur *capteur);
+//  void setComportement(unique_ptr<IComportement> comportement);
+//
+//  void action(Milieu &monMilieu);
+//
+//  bool jeTePercoit(const Bestiole &b) const;
+//  void changeState();
+//  void draw(UImg &support);
+//  double get_camouflage_coef() const;
+//  void set_camouflage_coef(double) { this->camouflage_coef = camouflage_coef; };
+//
+//  double getOrientation() const;
+//  void setOrientation(double o);
+//
+//  pair<double, double> getCoordinates() const;
+//
+//  void setVitesse(double o);
+//  double get_vitesse() const { return this->vitesse; };
+//  double get_max_vitesse() const { return this->max_vitesse; };
+//
+//  friend bool operator==(const Bestiole &b1, const Bestiole &b2);
+//
+//  void addAccessoire(double speed_multiplier, double fragility_multiplier,
+//                     double camouflage_mutliplier);
+//
+//  void set_fragility(double f) { this->fragility = f; };
+//  double get_fragility() { return this->fragility; };
+//
+//  void move_capteur(unique_ptr<ICapteur> &&cap);
+//
+//  bool isCollidingWith(Bestiole const &b) const;
+//  bool isDead() const;
+//  void kill();
+//
+//  string get_comportement_name() const;
 };
 
 bool operator!=(const Bestiole &b1, const Bestiole &b2);
